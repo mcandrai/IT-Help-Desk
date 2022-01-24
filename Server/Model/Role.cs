a9d5be00e0e9912c8e8213311582ade8f47654c0
VisensiusDony <46786595+VisensiusDony@users.noreply.github.com>
2022-01-24 15:12:55 +0700

    Add_Role_and_RolesCRUD

--- Server/Model/Role.cs
+++ Server/Model/Role.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Server.Model
 {
+    [Table("tb_m_roles")]
     public class Role
     {
         [Key]
